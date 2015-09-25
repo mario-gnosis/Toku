e251461e3146048213dfa4c0168040b23e877085
Mario <mario_gnosis@yahoo.com.br>
2015-09-25 12:30:47 -0400

    Initial commit

---
+++ app/src/main/java/com/support/android/designlibdemo/Tokusatsu.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.support.android.designlibdemo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Tokusatsu {
+
+    public static Map<String, String> lista = new HashMap<String, String>();
+
+      static {
+
+        lista.put("Jiraya", "jiraiya.png");
+        lista.put("Jaspion", "jaspion.png");
+        lista.put("Jiban", "jiban.png");
+        lista.put("Changem", "changem.png");
+        lista.put("Lion Man", "lion_man.png");
+        lista.put("Goggle Five", "goggle_five.png");
+        lista.put("Spectreman", "spectreman.png");
+        lista.put("Machine Man", "machine_man.png");
+        lista.put("Cybercops", "cybercops.png");
+        lista.put("Metalder", "metalder.png");
+        lista.put("Flashman", "flashman.png");
+        lista.put("Black Kamen Rider", "blck_kamen_rider.png");
+        lista.put("Ultraman", "ultraman.png");
+        lista.put("Sharivan", "sharivan.png");
+        lista.put("Gyaban", "gyaban.png");
+        lista.put("Spielvan", "spielvan.png");
+        lista.put("Winspector", "winspector.png");
+        lista.put("Solbrain", "solbrain.png");
+        lista.put("National Kid", "national_kid.png");
+        lista.put("Vingadores do Espaço", "vinagadores.png");
+        lista.put("Policial Espacial Shaider", "shaider.png");
+        lista.put("Robo Gigante", "robo_gigante.png");
+        lista.put("Esper ou Vesper", "esper.png");
+        lista.put("Ultraman Jack", "ultraman_jack.png");
+        lista.put("Lion Man Branco", "lion_braco.png");
+        lista.put("ByCrossers", "bycrossers.png");
+        lista.put("Maskman", "maskman.png");
+        lista.put("Ryakendo", "ryukendo.png");
+    }
+
+}
+
